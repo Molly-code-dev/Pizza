79705df3a755af5a08e18a675cb38c2b537e0371
Alvin-Karanja <alvin2@protonmail.com>
2019-08-31 14:58:15 +0300

    add order computation

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,42 @@ function Order (customSize, cheese) {
   this.pizzaPrice = 0;
   this.sidePrice = 3;
 }
+
+Order.prototype.pizzaCost = function () {
+  if (this.customSize === "Small 10 in.") {
+    this.pizzaPrice += 6;
+  } else if (this.customSize === "Medium 14 in.") {
+    this.pizzaPrice += 9;
+  } else if (this.customSize === "Large 18 in.") {
+    this.pizzaPrice += 12;
+  }
+  if (this.cheese === "cheese") {
+    this.pizzaPrice += 1;
+  } else if (this.cheese === "light cheese") {
+    this.pizzaPrice += 0.5;
+  } else if (this.cheese === "extra cheese") {
+    this.pizzaPrice += 1.5;
+  }
+  this.pizzaPrice += this.sauce;
+  this.pizzaPrice += this.veggie1;
+  this.pizzaPrice += this.veggie2;
+  this.pizzaPrice += this.meat;
+  return this.pizzaPrice;
+}
+Order.prototype.sideCost = function () {
+  return this.sidePrice;
+}
+Order.prototype.finalCost = function () {
+  var cartTotalPrice = 0;
+  for (var arrayElement = 0; arrayElement < totalPriceArray.length; arrayElement ++) {
+    cartTotalPrice += totalPriceArray[arrayElement]; //////////////////////IMPORTANT!!! How to add contents of an array together
+  }
+  return cartTotalPrice;
+}
+function Address (streetAddress, city, location, areacode) {
+  this.streetAddress = streetAddress;
+  this.city = city;
+  this.location = location;
+  this.areacode = areacode;
+  this.deliveryAddress = (streetAddress + "  " + city + ", " + location + "  " + areacode);
+}
