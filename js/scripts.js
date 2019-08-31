933d67812eb963de025b7fb8f9684dab0cce7e99
Alvin-Karanja <alvin2@protonmail.com>
2019-08-31 14:55:41 +0300

    add order function

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var totalPriceArray = []; //Only global variable in code
+function Order (customSize, cheese) {
+  this.customSize = customSize;
+  this.sauce = 1;
+  this.cheese = cheese;
+  this.veggie1 = 1;
+  this.veggie2 = 1;
+  this.meat = 2;
+  this.pizzaPrice = 0;
+  this.sidePrice = 3;
+}
